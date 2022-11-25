eb733d3d984a4d10a1bb2ae82c04754951232988
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-11-25 11:02:59 +0300

    Add method to generate computer choice

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello, World!");
+function getComputerChoice() {
+    let randNum = Math.floor(Math.random() * 3) + 1;
+
+    if (randNum === 1) {
+        return "Rock";
+    }
+    if (randNum === 2) {
+        return "Paper";
+    }
+
+    return "Scissors";
+}
+
+console.log(getComputerChoice());
